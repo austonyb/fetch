942aa5ee232d2f5d6a3957942a9fe2170fc02f2c
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2025-03-11 01:11:14 -0600

    feat: redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link"
-import { Button } from '@/components/ui/button'
-// import Marquee from '@/components/ui/marquee'
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-
-  // const items = ['Dogs!', 'More Dogs!', 'Fetch!', 'Neobrutalism is rad', 'Hi mom!']
-
-  return (
-    <div className="grid items-center justify-items-center p-8">
-      {/* <Marquee items={items} /> */}
-      <Link href="/login">
-        <Button className="w-full">Login</Button>
-      </Link>
-    </div>
-  );
+  // Redirect to login page
+  redirect('/login');
+  
+  // This code below won't execute due to the redirect
+  return null;
 }
